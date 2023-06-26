6e47c99ea57ab6c77e88cb92a5fd28b3905dda11
Diogo Lima <filosofo_carpediem@hotmail.com>
2023-06-26 10:06:23 -0300

    Spring Boot project created

---
+++ src/main/java/com/devsuperior/domainmodelorm/DomainmodelormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.domainmodelorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainmodelormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainmodelormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/domainmodelorm/DomainmodelormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.domainmodelorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainmodelormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
